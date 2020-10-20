134a545c7d9d70c6a55b847a4b9847e831ce1dec
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-10-19 23:55:22 -0400

    upload code

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	@g++ -Wall -g -c main.cpp -o luckcalc.o
+	@g++ -o luckcalc luckcalc.o   
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int bad_luck;
+	int good_luck;
+	//cout << bad_luck << ", " << good_luck << endl;
+
+	if (bad_luck > good_luck) {
+		cout << "bad luck";
+	} else {
+		cout << "good luck";
+	}
+	return 0;
+}
